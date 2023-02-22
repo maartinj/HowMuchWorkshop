5f069a5d1a2706705bab4eca57b97412c0567128
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-02-22 04:40:01 +0100

    Completed Coding Workshop Part 1 - Introduction

--- How Much/ContentView.swift
+++ How Much/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Color(.cyan)
+            Image(systemName: "archivebox.fill")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Click Me") {
+                
+            }
         }
+        .foregroundColor(.orange)
+        .border(.green)
         .padding()
+        .border(.purple)
     }
 }
 
