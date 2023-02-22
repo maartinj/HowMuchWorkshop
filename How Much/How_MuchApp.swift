f20311a84f4796699260d208df6c7154b9eb07af
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-02-22 03:51:36 +0100

    First commit - loaded initial project

---
+++ How Much/How_MuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_MuchApp.swift
+//  How Much
+//
+//  Created by Marcin Jędrzejak on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct How_MuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
